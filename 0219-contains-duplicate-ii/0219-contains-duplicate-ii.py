a331d2f7a5ec48759ca82a60da9355859e9236da
Adithya Raj Kolladath <kadithyaraj@gmail.com>
2023-09-17 01:09:55 +0530

    Time: 518 ms (73.00%), Space: 29.5 MB (71.31%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -2,11 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         hash_map = dict()
         for index, ele in enumerate(nums):
-            if hash_map.get(ele) is not None:
-                if abs(hash_map.get(ele) - index) <= k:
+            if hash_map.get(ele) is not None and abs(hash_map.get(ele) - index) <= k:
                     return True
-                else:
-                    hash_map[ele] = index
-            else:
-                hash_map[ele] = index
+            hash_map[ele] = index
         return False
