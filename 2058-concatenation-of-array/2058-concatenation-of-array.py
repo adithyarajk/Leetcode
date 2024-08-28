aa9528a13d1b672865521a28f8fe136d907a8868
Adithya Raj Kolladath <kadithyaraj@gmail.com>
2024-08-29 00:27:29 +0530

    Time: 49 ms (87.93%), Space: 11.8 MB (95.08%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        ans = nums + nums
+
+        return ans
+
+        
